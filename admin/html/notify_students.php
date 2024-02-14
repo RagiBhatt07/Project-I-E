0a5f996dddce69a86c6aaa504d404199bfd6f675
Ragi Bhatt <124009502+RagiBhatt07@users.noreply.github.com>
2024-02-14 15:55:23 +0100

    Update notify_students.php

--- admin/html/notify_students.php
+++ admin/html/notify_students.php
@@ -14,8 +14,8 @@ function notify_students_email($pdo, $courseID, $email_from) {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
         $mail->SMTPAuth = true; // Enable SMTP authentication
-        $mail->Username = 'sparkdelena@gmail.com'; // SMTP username
-        $mail->Password = 'ebiq aheg vkjn kgwg'; // SMTP password
+        $mail->Username = ''; // SMTP username
+        $mail->Password = ''; // SMTP password
         $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
         $mail->Port =  587; // TCP port to connect to
         $mail->IsHTML(true);
